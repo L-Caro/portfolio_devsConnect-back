c97866a86e6e8fb99e06ee956352c640a585ad70
Elyette-Gandji <elyette.gandji@gmail.com>
2023-06-12 11:28:25 +0200

    swagger params

--- app/index.js
+++ app/index.js
@@ -3,6 +3,31 @@ const router = require('./routers');
 
 const app = express();
 
+// SWAGGER
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "DevsConnect library API",
+			version: "1.0.0",
+			description: "Documentation for DevsConnect API",
+		},
+		servers: [
+			{
+				url: "http://localhost:3000",
+			},
+		],
+	},
+	apis: ["/routers/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 // mise en place des methodes json et URL encoded dans l'app de l'api
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
