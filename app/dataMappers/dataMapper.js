972bd6a9890cd847df5551c3e880ebe0fe8e857e
Pmangeot <pierre.mangeot@gmail.com>
2023-06-15 14:41:23 +0200

    verify and small change added

--- app/dataMappers/dataMapper.js
+++ app/dataMappers/dataMapper.js
@@ -152,18 +152,47 @@ const dataMapper = {
     return project;
   },
 
-  async updateOneProject (projectId, updatedFields) {
-    const {title, description, availability, user_id} = updatedFields;
-    const preparedQuery= {
-       text: `UPDATE "project" SET title = COALESCE($1, title), description = COALESCE($2, description), availability = COALESCE($3, availability), user_id = COALESCE($4, user_id), updated_at = NOW() WHERE id=$5 RETURNING *`,
-       values: [title, description, availability, user_id, projectId]
+  async updateOneProject(projectId, updatedFields) {
+    const { title, description, availability, user_id, tags, users } = updatedFields;
+    const preparedQuery = {
+      text: `UPDATE "project"
+             SET title = COALESCE($1, title),
+                 description = COALESCE($2, description),
+                 availability = COALESCE($3, availability),
+                 user_id = COALESCE($4, user_id),
+                 updated_at = NOW()
+             WHERE id = $5
+             RETURNING *`,
+      values: [title, description, availability, user_id, projectId]
+    };
+  
+    if (tags) {
+      preparedQuery.text += `; DELETE FROM "project_has_tag" WHERE project_id = $5;`;
+      for (const tag of tags) {
+        preparedQuery.text += ` INSERT INTO "project_has_tag" (project_id, tag_id) VALUES ($5, $${preparedQuery.values.length + 1})`;
+        // += permet de concaténer les requêtes
+        // $$ permet de délimiter une variable dans une requête
+        // $5 correspond à l'id du projet, $6 correspond à l'id du premier tag, $7 au deuxième, etc.
+        preparedQuery.values.push(tag);
+        // On ajoute l'id du tag dans le tableau des valeurs
+      }
     }
+  
+    if (users) {
+      preparedQuery.text += `; DELETE FROM "project_has_user" WHERE project_id = $5;`;
+      for (const user of users) {
+        preparedQuery.text += ` INSERT INTO "project_has_user" (project_id, user_id) VALUES ($5, $${preparedQuery.values.length + 1})`;
+        preparedQuery.values.push(user);
+      }
+    }
+  
     const results = await client.query(preparedQuery);
     if (!results.rows[0]) {
       throw new ApiError('Project not found', { statusCode: 204 });
     }
     return results.rows[0]; 
   },
+
 
 /// --- USER
 
